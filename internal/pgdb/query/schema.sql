b5ca8d5968bcc4f37538654acc186682fafaf7eb
ben-smyth <ben.df.smyth@gmail.com>
2024-03-24 23:01:31 +0000

    adding the start of oauth and also the schema for dappy

--- internal/pgdb/query/schema.sql
+++ internal/pgdb/query/schema.sql
@@ -1,7 +1,28 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS applications (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    public BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS environments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    application_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (application_id) REFERENCES applications (id)
+);
+
+CREATE TABLE IF NOT EXISTS teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS application_teams (
+    application_id INTEGER NOT NULL,
+    team_id INTEGER NOT NULL,
+    PRIMARY KEY (application_id, team_id),
+    FOREIGN KEY (application_id) REFERENCES applications (id),
+    FOREIGN KEY (team_id) REFERENCES teams (id)
 );
